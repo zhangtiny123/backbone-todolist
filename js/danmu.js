a8847d9148901d6ac43ad2869577d4f071a4024f
Tiny <tzhang920314@gmail.com>
2015-06-05 11:43:50 +0800

    integrated code

--- js/danmu.js
+++ js/danmu.js
@@ -1,54 +1,9 @@
 var DAN_MU_APP = {};
-var dan_mus = [{"text": "hahahaha", "id":"1", "color": "red", "size": "0", "position": "0"},
-    {"text": "233333", "id":"2", "color": "red", "size": "0", "position": "2"}, {
-        "text": "poi",
-        "id": "13",
-        "color": "red",
-        "size": "1",
-        "position": "1"
-    },
-    {"text": "2333", "id":"3", "color": "#FFFFFF ", "size": "0", "position": "0"}, {
-        "text": "XXX真好",
-        "id":"4",
-        "color": "#FFFFFF ",
-        "size": "0",
-        "position": "2"
-    }, {"text": "你个傻叉", "id":"5", "color": "red", "size": "0", "position": "0"},
-    {"text": "233333", "id":"6", "color": "red", "size": "0", "position": "2"}, {
-        "text": "切",
-        "id":"7",
-        "color": "red",
-        "size": "1",
-        "position": "1"
-    },
-    {"text": "2333", "id":"8", "color": "#FFFFFF ", "size": "0", "position": "0"}, {
-        "text": "每一他",
-        "id":"14",
-        "color": "#FFFFFF ",
-        "size": "0",
-        "position": "2"
-    }, {"text": "人生哈", "id":"9", "color": "red", "size": "0", "position": "0"},
-    {"text": "233333", "id":"15", "color": "red", "size": "0", "position": "2"}, {
-        "text": "asdfe",
-        "id":"10",
-        "color": "red",
-        "size": "1",
-        "position": "1"
-    },
-    {"text": "2333", "id":"11", "color": "#FFFFFF ", "size": "0", "position": "0"}, {
-        "text": "asdfafeqewtqg",
-        "id":"12",
-        "color": "#FFFFFF ",
-        "size": "0",
-        "position": "2"
-    }];
-
+DAN_MU_APP.timestamp = null;
+DAN_MU_APP.dan_mus = [];
+DAN_MU_APP.interval_for_barrage_appear = -1;
+DAN_MU_APP.interval_for_fetch_data = -1;
 DAN_MU_APP.cache_comment = [];
-DAN_MU_APP.refreshId = -1;
-
-DAN_MU_APP.getCommentsFromServer = function() {
-    return dan_mus;
-};
 
 DAN_MU_APP.getSelfInput = function(e) {
     var input = $("#comment_input");
@@ -90,13 +45,12 @@ $(".switch_button").on("click", function() {
 });
 
 function start() {
-    var commentForShow = DAN_MU_APP.getCommentsFromServer();
     if (DAN_MU_APP.cache_comment.length !== 0) {
-        commentForShow.push(DAN_MU_APP.cache_comment.pop())
+        DAN_MU_APP.dan_mus.push(DAN_MU_APP.cache_comment.pop())
     }
-    var data = commentForShow.pop();
+    var data = DAN_MU_APP.dan_mus.pop();
     if (data !== undefined) {
-        var span = "<span style=\"position:absolute;transform:translate(0," + data.position * 50 + "px);animation:my_move 10s linear 1;\">" + data.text + "</span>";
+        var span = "<span style=\"position:absolute;transform:translate(0," + data.position * 50 + "px);animation:my_move 10s linear 1;\">" + data.content + "</span>";
         var danMuParent = $("#dan_mu_parent");
         $(span).appendTo(danMuParent);
         danMuParent.on('webkitAnimationEnd oanimationend msAnimationEnd animationend', "span", function () {
@@ -105,16 +59,54 @@ function start() {
     }
 }
 
+function getBarrageData() {
+    //var url = 'http://10.29.2.253:8080/casaDS/barrage/get_barrages.ds?listingId='+$("#emailListingIdFragment")[0].value+"&timestamp"+DAN_MU_APP.timestamp;
+    var url = 'http://10.29.2.253:8080/casaDS/barrage/get_barrages.ds?listingId=22000053';
+    $.ajax({
+        type: 'GET',
+        url: url,
+        async: false,
+        dataType: 'jsonp',
+        jsonpCallback: 'jsonCallback',
+        contentType: "application/json",
+        success: function (response) {
+            DAN_MU_APP.timestamp = response.timestamp;
+            DAN_MU_APP.dan_mus = response.results.concat(DAN_MU_APP.dan_mus);
+        }
+
+    });
+}
+
+function saveBarrageData() {
+    var url = 'http://10.29.2.253:8080/casaDS/barrage/add_barrage.ds?listingId='+$("#emailListingIdFragment")[0].value+"&dataType="+dataType()+"&text="+comment.text+"&position="+comment.position;
+    $.ajax({
+        type: 'GET',
+        url: url,
+        async: false,
+        dataType: 'jsonp',
+        jsonpCallback: 'jsonCallback',
+        contentType: "application/json",
+        success: function (result) {
+            DAN_MU_APP.dan_mus = DAN_MU_APP.dan_mus.concat(result.results);
+        }
+    });
+}
+
 DAN_MU_APP.start_dan_mu = function() {
     $("#dan_mu_parent").removeAttr("hidden");
-    DAN_MU_APP.refreshId = setInterval(function () {
+    getBarrageData();
+    DAN_MU_APP.interval_for_barrage_appear = setInterval(function () {
         start();
     }, 1000);
+    DAN_MU_APP.interval_for_fetch_data = setInterval(function () {
+        getBarrageData();
+    }, 10000);
 };
 
 DAN_MU_APP.stop_dan_mu = function() {
     var parent = $("#dan_mu_parent");
     parent.find("span").remove();
     parent.attr("hidden", "hidden");
-    clearInterval(DAN_MU_APP.refreshId);
+    clearInterval(DAN_MU_APP.interval_for_barrage_appear);
+    clearInterval(DAN_MU_APP.interval_for_fetch_data);
 };
